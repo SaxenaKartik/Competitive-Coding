7ac81a597f5917c76f8d29fd07c2846f0ff7e907
kartik <kartiksaxena500@gmail.com>
2018-10-30 23:23:24 +0530

    Added ICPC folder

---
+++ ICPC/WORDGRID.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define debug(x) do { std::cerr << #x << ": " << x << std::endl; } while (0)
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+
+#define ll long long
+
+ll n;
+
+int32_t main()
+{
+	int t;
+	while()	
+}
