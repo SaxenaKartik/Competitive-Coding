bf7c5719cddb3f6ff98dc6714939a95a55502995
kartik <kartiksaxena500@gmail.com>
2018-10-31 01:04:22 +0530

    Added some imp files

---
+++ test.cpp
@@ -0,0 +1,22 @@
+// C++ program to calculate random numbers using LCM method
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifndef ONLINE_JUDGE
+#include "prettyprint.hpp"
+#define debug(...) cout << "debug: " << #__VA_ARGS__ " = " << make_tuple(__VA_ARGS__) << endl
+#else
+#define debug(...) 0
+#endif
+typedef long long int ll;
+#define int ll
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int32_t main()
+{
+	
+}
